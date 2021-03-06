8be5c8ddfb3ac61771c99557b0a60cb09453dc93
ok克里斯汀 <402605042@qq.com>
2021-03-06 09:30:26 +0800

    修改 城市

--- weather.js
+++ weather.js
@@ -4,7 +4,7 @@
 */
 const $ = new Env('墨迹天气');
 const notify = $.isNode() ? require('./sendNotify') : '';
-const City = encodeURIComponent($.getdata('city') || "北京") //可在此处修改城市
+const City = encodeURIComponent($.getdata('city') || "杭州") //可在此处修改城市
 const j = $.getdata('citynum') || "1";
 let reduction = $.getdata('cut') || 'false'; //日志
 let daylys = $.getdata('day_desc') || 'true', //每日天气
